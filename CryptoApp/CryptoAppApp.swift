f57bc8d2d110c46e60640f8b60456d010e82bc6b
tayyabmughal676 <tayyabmughal676@gmail.com>
2021-08-03 23:03:28 +0500

    Initial Commit

---
+++ CryptoApp/CryptoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAppApp.swift
+//  CryptoApp
+//
+//  Created by Thor on 03/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct CryptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
